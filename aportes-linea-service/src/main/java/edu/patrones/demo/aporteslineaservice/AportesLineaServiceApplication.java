26b2e615a82c75b8afde434625fb8abab428fed7
Tito Andrés Maturana de la Cruz <tamatu@gmail.com>
2022-05-12 12:34:19 -0500

    Aportes En Línea Service

---
+++ aportes-linea-service/src/main/java/edu/patrones/demo/aporteslineaservice/AportesLineaServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.patrones.demo.aporteslineaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AportesLineaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AportesLineaServiceApplication.class, args);
+    }
+
+}
---
+++ aportes-linea-service/src/test/java/edu/patrones/demo/aporteslineaservice/AportesLineaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.patrones.demo.aporteslineaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AportesLineaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
