c13b7b9fea80946f8657be91656e8804027c7b94
Tito Andrés Maturana de la Cruz <tamatu@gmail.com>
2022-05-12 18:56:00 -0500

    Estudio Solicitud Service

---
+++ estudio-solicitud-service/src/main/java/edu/patrones/demol/estudiosolicitudservice/EstudioSolicitudServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.patrones.demol.estudiosolicitudservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudioSolicitudServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstudioSolicitudServiceApplication.class, args);
+    }
+
+}
---
+++ estudio-solicitud-service/src/test/java/edu/patrones/demol/estudiosolicitudservice/EstudioSolicitudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.patrones.demol.estudiosolicitudservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudioSolicitudServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
