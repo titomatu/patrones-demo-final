c9f756349a906af1d2cbfddc01e95871c748cf58
Tito Andrés Maturana de la Cruz <tamatu@gmail.com>
2022-05-14 21:01:33 -0500

    Eureka Server

---
+++ eureka-service/src/main/java/edu/patrones/demo/eurekaservice/EurekaServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.patrones.demo.eurekaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceApplication.class, args);
+	}
+
+}
