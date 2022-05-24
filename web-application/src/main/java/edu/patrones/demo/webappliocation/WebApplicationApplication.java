a1fef7048c6abd70c248074a405a8b2c135bad91
Tito Andrés Maturana de la Cruz <tamatu@gmail.com>
2022-05-24 08:27:16 -0500

    Web Application

---
+++ web-application/src/main/java/edu/patrones/demo/webappliocation/WebApplicationApplication.java
@@ -0,0 +1,13 @@
+package edu.patrones.demo.webappliocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplicationApplication.class, args);
+    }
+
+}
---
+++ web-application/src/test/java/edu/patrones/demo/webappliocation/WebApplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.patrones.demo.webappliocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
