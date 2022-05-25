e5f722ce00d416a80c89de27ed8c4bf684b48d47
Tito Andrés Maturana de la Cruz <tamatu@gmail.com>
2022-05-24 20:31:21 -0500

    Web Application

--- web-application/src/main/java/edu/patrones/demo/webappliocation/controller/WebApplicationController.java
+++ web-application/src/main/java/edu/patrones/demo/webappliocation/controller/WebApplicationController.java
@@ -1,13 +1,26 @@
 package edu.patrones.demo.webappliocation.controller;
 
+import edu.patrones.demo.dto.OTPRequestDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class WebApplicationController {
 
     @RequestMapping("/")
-    public String index(){
-        return "index";
+    public String index(Model model){
+        //return "index";
+        OTPRequestDto otpRequestDto = new OTPRequestDto();
+        model.addAttribute("otpRequestDto", otpRequestDto);
+
+        return "otp";
+    }
+
+    @GetMapping("/otp")
+    public String otp(){
+        return "otp";
     }
 }
